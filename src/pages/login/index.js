5f5ea50a1036620043355d6a8a66c20787b56bbf
thfrod <thiago.ferreirarod22@gmail.com>
2021-07-13 14:59:58 -0300

    Finalização no desenvolvimento da tela de login (1° tela)

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import './login.css'
-import {MdEmail, MdLock} from 'react-icons/md'
-
-function Login(){
-    return(
-        <div className="Login">
-            
-            <div className="login-logo">
-                
-                <img src="https://anzuns.org/wp-content/uploads/2018/02/admin_login.png" alt="Logo do Sopha" />
+import React from "react";
+import "./login.css";
+//import { MdEmail, MdLock } from "react-icons/md";
+ 
+function Login() {
+  return (
+    <main className="Login">
+        <div className="painel">
+            <div className="logo">
+                <img src="https://planejados.sopha.com.br/images/logo.png"alt="Logo do Sopha"/>
             </div>
-
-            <div className="login-right">
-                <h1>Acessar App</h1>
+            <form action="">
                 <div className="loginInputEmail">
-                    <MdEmail/>
-                    <input type="text" placeholder="Digite seu e-mail" />
+                    <input type="text" placeholder="digite seu e-mail" />
                 </div>
 
                 <div className="loginInputPassword">
-                    <MdLock/>
-                    <input type="password" placeholder="Digite sua senha" />
+                    <input type="password" placeholder="digite sua senha" />
                 </div>
 
-                <button type="submit">Entrar</button>
+                <input type="submit" value="Entrar" id="button"/>
 
-                <h4>Não tenho conta!</h4>
-
-                <button type="submit">Cadastrar</button>
-            </div>
+                <div className="loginEsqueciSenha">
+                    <a href="#">esqueci minha senha</a>
+                </div>
+            </form>
+            
         </div>
-    )
+        <div className="rodapeDireitos">
+            <p>Sopha © 2021</p>
+        </div>
+    </main>
+  );
 }
 
 export default Login;
